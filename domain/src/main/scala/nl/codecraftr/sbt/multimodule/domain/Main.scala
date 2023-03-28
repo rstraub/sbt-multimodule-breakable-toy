d7f6420e4b4ebdfea7555d8c3b391831660b67e3
Roy Straub <roy@codecraftr.nl>
2023-03-28 13:31:18 +0200

    Import

--- domain/src/main/scala/nl/codecraftr/sbt/multimodule/domain/Main.scala
+++ domain/src/main/scala/nl/codecraftr/sbt/multimodule/domain/Main.scala
@@ -1,5 +1,7 @@
 package nl.codecraftr.sbt.multimodule.domain
 
+import nl.codecraftr.sbt.multimodule.core.Util.pretty
+
 object Main {
   def main(args: Array[String]): Unit = {
     println(pretty("  Hello domain!   "))
