e16833c9f3aff0698992cee8263122b5e41a8aa4
Roy Straub <roy@codecraftr.nl>
2023-03-28 17:39:13 +0200

    Also run scalafmt

--- domain/src/main/scala/nl/codecraftr/sbt/multimodule/domain/Main.scala
+++ domain/src/main/scala/nl/codecraftr/sbt/multimodule/domain/Main.scala
@@ -3,7 +3,7 @@ package nl.codecraftr.sbt.multimodule.domain
 import nl.codecraftr.sbt.multimodule.core.Util.pretty
 
 object Main extends App {
-  private var foo = 4
+    private var foo = 4
 
   println(pretty("  Hello domain!   "))
 }
